d783fcc0b86d46b1038c8fc870813733c246db7c
Andrei Podgornyi <andrei.podgornyi@appus.me>
2018-10-11 19:31:35 +0300

    [+] add customer page to menu

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,6 +1,21 @@
 <li class="{{\Route::current()->getName() === 'admin' ? 'active' : ''}}">
     <a href="{{route('admin')}}"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a>
 </li>
+<li class="treeview {{ActiveLink::checkManagement() ? 'active' : ''}}">
+    <a href="#" class="treeview-toggle"><i class="fa fa-table"></i><span>Management</span>
+        <span class="pull-right-container">
+                <i class="fa fa-angle-left pull-right"></i>
+              </span>
+    </a>
+    <ul class="treeview-menu">
+        <li class="{{ActiveLink::checkCustomers() ? 'active' : ''}}">
+            <a href="{{url('/admin/customers')}}"><i class="fa fa-circle-o"></i>Customers</a>
+        </li>
+        <li class="{{ActiveLink::checkMerchants() ? 'active' : ''}}">
+            <a href="{{url('/admin/merchants')}}"><i class="fa fa-circle-o"></i>Merchants</a>
+        </li>
+    </ul>
+</li>
 <li class="{{\Route::current()->getName() === 'content' ? 'active' : ''}}">
     <a href="{{route('content')}}"><i class="mdi mdi-book-open-page-variant"></i> <span>Terms & Condiotions</span></a>
 </li>
