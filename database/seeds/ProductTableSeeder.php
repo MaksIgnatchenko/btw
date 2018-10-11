66cdc26484c3690955147ca41c1105bda3e0916b
ilyakobus <ilya.kobus@appus.me>
2018-10-11 11:51:01 +0300

    [#] Updated Products seeed

--- database/seeds/ProductTableSeeder.php
+++ database/seeds/ProductTableSeeder.php
@@ -15,12 +15,12 @@ class ProductTableSeeder extends Seeder
     {
         $categorieIds = [12, 6, 7, 8, 9];
 
-        for ($i = 1; $i <= 10; $i++) {
+        for ($i = 1; $i <= 20; $i++) {
             DB::table('products')->insert([
                 'name' => 'Test Product #' . $i,
                 'description' => 'Test Product #' . $i . ' Desc',
-                'price' => mt_rand(20, 1000) + mt_rand(0, 99) / 100,
-                'quantity' => mt_rand(1, 20),
+                'price' => mt_rand(100, 2000) + mt_rand(0, 99) / 100,
+                'quantity' => mt_rand(1, 50),
                 'category_id' => $categorieIds[mt_rand(0, count($categorieIds) - 1)],
                 'main_image' => 'demo.jpeg',
                 'created_at' => Carbon::now(),
