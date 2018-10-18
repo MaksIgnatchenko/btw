40635fb45c2a64962c3bead03a4f6f36e573d6a8
Andrei Podgornyi <andrei.podgornyi@appus.me>
2018-10-18 13:48:14 +0300

    [+] create address model

--- app/Modules/Users/Merchant/Models/Address.php
+++ app/Modules/Users/Merchant/Models/Address.php
@@ -3,15 +3,19 @@
  * Created by Ilya Kobus, Appus Studio LP on 16.10.2018
  */
 
-namespace App\Modules\Users\Models;
+namespace App\Modules\Users\Merchant\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Address extends Model
 {
-    protected $guarded = [];
-
-    public function merchant()
+    /**
+     * Address to merchant relation.
+     *
+     * @return BelongsTo
+     */
+    public function merchant(): BelongsTo
     {
         return $this->belongsTo(Merchant::class);
     }
