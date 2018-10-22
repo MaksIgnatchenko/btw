b5ab788cff2a73828c4a462cf0c29fda0651f4c9
ilyakobus <ilya.kobus@appus.me>
2018-10-22 15:20:41 +0300

    [+] Created GeographyHelper

---
+++ app/Modules/Users/Merchant/Helpers/GeographyHelper.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by Ilya Kobus, Appus Studio LP on 19.10.2018
+ */
+
+namespace App\Modules\Users\Merchant\Helpers;
+
+use App\Modules\Users\Merchant\Enums\GeographyObjectTypesEnum;
+use App\Modules\Users\Merchant\Exceptions\UnknownGeographicObjectTypeException;
+use App\Modules\Users\Merchant\Services\Geography\GeographyServiceInterface;
+use Illuminate\Database\Eloquent\Collection;
+
+abstract class GeographyHelper
+{
+    public static function getObjectsByParentId(int $parentId, string $objectType): Collection
+    {
+        $geographyService = app()[GeographyServiceInterface::class];
+
+        switch ($objectType) {
+            case GeographyObjectTypesEnum::STATE:
+                return $geographyService->getStates($parentId);
+            case GeographyObjectTypesEnum::CITY:
+                return $geographyService->getCities($parentId);
+            default:
+                throw new UnknownGeographicObjectTypeException();
+        }
+    }
+
+}
