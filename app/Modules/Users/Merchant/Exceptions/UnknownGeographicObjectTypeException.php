bcccabf8d7f5f6ca923013fea919f2009974fd7f
ilyakobus <ilya.kobus@appus.me>
2018-10-22 15:22:26 +0300

    [+] Created Geography exception UnknownGeographicObjectTypeException

---
+++ app/Modules/Users/Merchant/Exceptions/UnknownGeographicObjectTypeException.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by Ilya Kobus, Appus Studio LP on 19.10.2018
+ */
+
+namespace App\Modules\Users\Merchant\Exceptions;
+
+class UnknownGeographicObjectTypeException extends \Exception
+{
+
+}
