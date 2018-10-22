5f5584839f29b1a61eb53511a2e40702fafaf676
ilyakobus <ilya.kobus@appus.me>
2018-10-22 15:17:40 +0300

    [+] Created Geography facade for form`s selects

---
+++ app/Modules/Users/Merchant/Facades/Geography.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by Ilya Kobus, Appus Studio LP on 22.10.2018
+ */
+
+namespace App\Modules\Users\Merchant\Facades;
+
+use App\Modules\Users\Merchant\Services\Geography\GeographyServiceInterface;
+use Illuminate\Support\Facades\Facade;
+
+class Geography extends Facade
+{
+    protected const DEFAULT_CITY_RESULT = ['Cities'];
+    protected const DEFAULT_STATE_RESULT = ['States'];
+
+    protected static function getFacadeAccessor()
+    {
+        return GeographyServiceInterface::class;
+    }
+
+    public static function statesSelectValues(int $country = null)
+    {
+        $states = static::getFacadeRoot()->getStates($country)->pluck('name', 'id');
+
+        return $states->count() ? $states : self::DEFAULT_STATE_RESULT;
+    }
+
+    public static function citiesSelectValues(int $state = null)
+    {
+        $cities = static::getFacadeRoot()->getCities($state)->pluck('name', 'id');
+
+        return $cities->count() ? $cities : self::DEFAULT_CITY_RESULT;
+    }
+}
