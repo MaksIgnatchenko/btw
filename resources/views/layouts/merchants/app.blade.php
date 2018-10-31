76e4c489ded2e8b8412caa5e952cfa67a95e8384
Andrei Podgornyi <andrei.podgornyi@appus.me>
2018-10-31 15:54:22 +0200

    [#] refactor main app layout

--- resources/views/layouts/merchants/app.blade.php
+++ resources/views/layouts/merchants/app.blade.php
@@ -13,33 +13,14 @@
 
     @yield('css')
 </head>
-<body>
+<body class="@yield('body-class')">
 <!-- Header -->
-<header class="header">
-    <div class="container">
-        <div class="header__cont">
-            <div class="header__logo">
-                <a href="{{route('index')}}">Logotype BTW</a>
-            </div>
-            <div class="header__info">
-                <div class="header__lang"><span>EN</span>English</div>
-                <nav class="navigation">
-
-                    @if (Route::currentRouteName() !== 'index')
-                        <a href="#">{{__('merchants.home')}}</a>
-                    @endif
-
-                    <a href="#">{{__('merchants.terms_and_conditions')}}</a>
-                </nav>
-            </div>
-        </div>
-    </div>
-</header><!-- /. end header -->
+@yield('header')
 
 @yield('content')
 
 <!-- Footer -->
-<footer class="footer">
+<footer class="@yield('footer-class')">
     <div class="container">
         <p class="footer__copy">&copy; {{date('Y')}} Better than Wish</p>
     </div>
