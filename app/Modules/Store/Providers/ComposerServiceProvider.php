d2668fc59a80b1d576b8281f0bf96838d09a31d7
Andrei Podgornyi <andrei.podgornyi@appus.me>
2018-10-31 16:00:44 +0200

    [+] create composer service provider

---
+++ app/Modules/Store/Providers/ComposerServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by Andrei Podgornyi, Appus Studio LP on 31.10.2018
+ */
+
+namespace App\Modules\Store\Providers;
+
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\ServiceProvider;
+
+class ComposerServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        View::composer('store.store', 'App\Modules\Store\Http\ViewComposers\StoreComposer');
+    }
+}
