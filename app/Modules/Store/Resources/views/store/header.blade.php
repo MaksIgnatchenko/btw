563f87db4b297f847cd751840f01626dcbd218dc
Andrei Podgornyi <andrei.podgornyi@appus.me>
2018-10-31 16:25:16 +0200

    [+] create merchant full name mutator

--- app/Modules/Store/Resources/views/store/header.blade.php
+++ app/Modules/Store/Resources/views/store/header.blade.php
@@ -7,7 +7,7 @@
             <div class="header-shop__info">
                 <div class="header__lang"><span></span>English</div>
                 <div class="header-shop__user-info">
-                    <div class="user__name">{{ MerchantHelper::getFullName($merchant) }}</div>
+                    <div class="user__name">{{ $merchant->fullName }}</div>
                     <div class="user__icon">
                         <figure class="user-icon__figure">
                             <img src="img/user-icon.png" alt="User icon">
