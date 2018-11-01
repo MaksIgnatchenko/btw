7ce01782760d9fcecc56aba62d57dc508ad0cdd4
ilyakobus <ilya.kobus@appus.me>
2018-11-01 19:41:30 +0200

    [#] Cart validation replaced to trait

---
+++ app/Modules/Products/Requests/Api/CartValidatorTrait.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by Ilya Kobus, Appus Studio LP on 1.11.2018
+ */
+
+namespace App\Modules\Products\Requests\Api;
+
+use App\Modules\Products\Models\Product;
+use App\Modules\Products\Repositories\CartRepository;
+use App\Modules\Users\Customer\Models\Customer;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Support\Facades\Auth;
+
+trait CartValidatorTrait
+{
+    /**
+     * @return Validator
+     */
+    protected function validatorAfter($validator)
+    {
+        $validator->after(function () use ($validator) {
+            /** @var Customer $customer */
+            $customer = Auth::user();
+            /** @var CartRepository $cartRepository */
+            $cartRepository = app(CartRepository::class);
+
+            $carts = $cartRepository->findCartsWithProducts($customer->id);
+
+            foreach ($carts as $cart) {
+                /** @var Product $product */
+                $product = $cart->product;
+                $productFromCartName = $cart->product['name'];
+
+                $productQuantity = $product->quantity;
+
+                if ($cart->quantity > $productQuantity) {
+                    $message = "Quantity {$cart->quantity} isn’t available for item {$productFromCartName}. Please decrease quantity to {$productQuantity} to proceed.";
+                    $validator->errors()->add($cart->id, $message);
+                }
+            }
+        });
+    }
+}
