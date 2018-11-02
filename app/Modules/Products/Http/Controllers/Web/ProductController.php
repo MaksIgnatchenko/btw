303e6751ad6888e3bcc1d62943288c393384fbf2
Andrei Podgornyi <andrei.podgornyi@appus.me>
2018-11-02 18:51:59 +0200

    [+] add new methods to product controller

--- app/Modules/Products/Http/Controllers/Web/ProductController.php
+++ app/Modules/Products/Http/Controllers/Web/ProductController.php
@@ -6,13 +6,27 @@
 namespace App\Modules\Products\Http\Controllers\Web;
 
 use App\Modules\Categories\Models\Category;
+use App\Modules\Products\Models\Product;
 use App\Modules\Products\Requests\Web\CreateProductRequest;
 use App\Http\Controllers\Controller;
 use Illuminate\View\View;
 
 class ProductController extends Controller
 {
+    protected $productModel;
+
     /**
+     * ProductController constructor.
+     * @param Product $product
+     */
+    public function __construct(Product $product)
+    {
+        $this->productModel = $product;
+    }
+
+    /**
+     * Show products list.
+     *
      * @return View
      */
     public function index(): View
@@ -21,17 +35,27 @@ class ProductController extends Controller
     }
 
     /**
+     * Show add new product page.
+     *
      * @return View
      */
     public function create(): View
     {
-        $categories = Category::all()->pluck('name', 'id');
+        $categories = Category::where('is_final', '1')->pluck('name', 'id');
 
         return view('products.web.create', ['categories' => $categories]);
     }
 
+    /**
+     * Create new product.
+     *
+     * @param CreateProductRequest $request
+     * @return CreateProductRequest
+     */
     public function store(CreateProductRequest $request)
     {
+        $this->productModel->createProduct($request->all());
+
         return $request;
     }
 }
