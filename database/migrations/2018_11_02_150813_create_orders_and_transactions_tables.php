19c27be42427f1fdc61a4b692fff96cfd23ded85
ilyakobus <ilya.kobus@appus.me>
2018-11-05 16:00:54 +0200

    [+] Add migration for creating orders and transactions tables

---
+++ database/migrations/2018_11_02_150813_create_orders_and_transactions_tables.php
@@ -0,0 +1,87 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersAndTransactionsTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::transaction(function() {
+            Schema::create('transactions', function (Blueprint $table) {
+                $table->increments('id');
+
+                $table->unsignedInteger('customer_id');
+
+                $table->foreign('customer_id')
+                    ->references('id')
+                    ->on('customers')
+                    ->onDelete('cascade');
+
+                $table->json('cart');
+                $table->text('message');
+                $table->string('status');
+                $table->decimal('amount');
+
+                $table->timestamps();
+            });
+
+            Schema::create('orders', function (Blueprint $table) {
+                $table->increments('id');
+
+                $table->unsignedInteger('customer_id');
+                $table->foreign('customer_id')
+                    ->references('id')
+                    ->on('customers')
+                    ->onDelete('cascade');
+
+                $table->unsignedInteger('merchant_id');
+                $table->foreign('merchant_id')
+                    ->references('id')
+                    ->on('merchants')
+                    ->onDelete('cascade');
+
+                $table->unsignedInteger('transaction_id');
+                $table->foreign('transaction_id')
+                    ->references('id')
+                    ->on('transactions')
+                    ->onDelete('cascade');
+
+                $table->json('product');
+
+                $table->integer('quantity');
+                $table->enum('status', array_keys(\App\Modules\Orders\Enums\OrderStatusEnum::toArray()));
+
+                $table->timestamps();
+            });
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropForeign('orders_customer_id_foreign');
+            $table->dropForeign('orders_merchant_id_foreign');
+            $table->dropForeign('orders_transaction_id_foreign');
+        });
+
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->dropForeign('transactions_customer_id_foreign');
+        });
+
+        Schema::dropIfExists('orders');
+        Schema::dropIfExists('transactions');
+    }
+}
