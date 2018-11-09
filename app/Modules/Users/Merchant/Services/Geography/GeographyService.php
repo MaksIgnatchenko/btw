72c78d531328e1ec746543d98f1064d2f7be08af
ilyakobus <ilya.kobus@appus.me>
2018-11-09 12:47:47 +0200

    [+] Increase Geography service methods

--- app/Modules/Users/Merchant/Services/Geography/GeographyService.php
+++ app/Modules/Users/Merchant/Services/Geography/GeographyService.php
@@ -58,4 +58,48 @@ class GeographyService implements GeographyServiceInterface
 
         return GeographyState::all();
     }
+
+    /**
+     * @param string   $name
+     * @param int|null $state
+     *
+     * @return GeographyCity
+     */
+    public function getCityByName(string $name, int $state = null): GeographyCity
+    {
+        $cities = (new GeographyCity)->newQuery();
+
+        if($state) {
+            $cities->where('state_id', $state);
+        }
+
+        return $cities->where('name', $name)->firstOrFail();
+    }
+
+    /**
+     * @param string $shortName
+     *
+     * @return GeographyCountry|null
+     */
+    public function getCountryByShortName(string $shortName): GeographyCountry
+    {
+        return GeographyCountry::where('sortname', $shortName)->firstOrFail();
+    }
+
+    /**
+     * @param string   $name
+     * @param int|null $country
+     *
+     * @return GeographyState
+     */
+    public function getStateByName(string $name, int $country = null): GeographyState
+    {
+        $states = (new GeographyState)->newQuery();
+
+        if($states) {
+            $states->where('country_id', $country);
+        }
+
+        return $states->where('name', $name)->firstOrFail();
+    }
 }
--- app/Modules/Users/Merchant/Services/Geography/GeographyServiceInterface.php
+++ app/Modules/Users/Merchant/Services/Geography/GeographyServiceInterface.php
@@ -5,7 +5,9 @@
 
 namespace App\Modules\Users\Merchant\Services\Geography;
 
+use App\Modules\Users\Merchant\Models\Geography\GeographyCity;
 use App\Modules\Users\Merchant\Models\Geography\GeographyCountry;
+use App\Modules\Users\Merchant\Models\Geography\GeographyState;
 use Illuminate\Database\Eloquent\Collection;
 
 interface GeographyServiceInterface
@@ -35,4 +37,28 @@ interface GeographyServiceInterface
      * @return Collection
      */
     public function getCities(int $state = null): Collection;
+
+    /**
+     * @param string $shortName
+     *
+     * @return GeographyCountry|null
+     */
+    public function getCountryByShortName(string $shortName): GeographyCountry;
+
+    /**
+     * @param string   $name
+     * @param int|null $country
+     *
+     * @return GeographyState
+     */
+    public function getStateByName(string $name, int $country = null): GeographyState;
+
+    /**
+     * @param string   $name
+     * @param int|null $state
+     *
+     * @return GeographyCity
+     */
+    public function getCityByName(string $name, int $state = null): GeographyCity;
+
 }
