e98f8cfd6988212d311690c0472041b16ebbe4a9
ilyakobus <ilya.kobus@appus.me>
2018-11-09 12:49:04 +0200

    [+] Add new JS, update CSS

---
+++ resources/views/layouts/merchants/navigation.blade.php
@@ -0,0 +1,14 @@
+<!-- Navigate Link -->
+<div class="top-nav-line">
+    <ul class="nav-line__list container">
+        <li class="nav-line__item">
+            <a href="{{ route('products.index') }}" class="nav-line__link @if ($active === 'products') nav-line__link--active @endif">{{__('store.my_products')}}</a>
+        </li>
+        <li class="nav-line__item">
+            <a href="#" class="nav-line__link @if ($active === 'orders') nav-line__link--active @endif">{{__('store.my_orders')}}</a>
+        </li>
+        <li class="nav-line__item">
+            <a href="#" class="nav-line__link @if ($active === 'settings') nav-line__link--active @endif">{{__('store.settings')}}</a>
+        </li>
+    </ul>
+</div><!-- /. end navigate link -->
