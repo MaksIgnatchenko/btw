9cda4753180d3ed1cc908fbd5b41c10800a7df80
Andrei Podgornyi <andrei.podgornyi@appus.me>
2018-11-12 14:02:49 +0200

    [!] fix attributes store

---
+++ app/Modules/Products/Helpers/AttributesHelper.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by Andrei Podgornyi, Appus Studio LP on 12.11.2018
+ */
+
+namespace App\Modules\Products\Helpers;
+
+use App\Modules\Categories\Enums\AttributeTypesEnum;
+
+class AttributesHelper
+{
+    public static function mergeAttributes(?array $attributes)
+    {
+        $mergedAttributes = [];
+
+        if ($attributes === null) {
+            return null;
+        }
+
+        foreach ($attributes as $type => $attributeArray) {
+            if (AttributeTypesEnum::check($type)) {
+                $mergedAttributes = array($attributeArray);
+            }
+
+            return $mergedAttributes;
+        }
+    }
+}
