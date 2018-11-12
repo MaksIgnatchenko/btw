12d11900a101bd60b3a01cca6cd8ed695403d868
ilyakobus <ilya.kobus@appus.me>
2018-11-12 17:09:10 +0200

    [-] Remove odd routes

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -68,35 +68,12 @@ Breadcrumbs::register('about-us', function ($breadcrumbs) {
     $breadcrumbs->push('About Us', route('content.about-us'));
 });
 
-// Admin panel > Product reviews
-Breadcrumbs::register('product-reviews', function ($breadcrumbs) {
-    $breadcrumbs->parent('admin');
-    $breadcrumbs->push('Product review', route('review.products.index'));
-});
-
-// Admin panel > Product reviews > [Product review]
-Breadcrumbs::register('product-review', function ($breadcrumbs, $productReview) {
-    $breadcrumbs->parent('product-reviews');
-    $breadcrumbs->push('Edit product review', route('review.products.view', ['id' => $productReview->id]));
-});
-
-// Admin panel > Merchant reviews
-Breadcrumbs::register('merchant-reviews', function ($breadcrumbs) {
-    $breadcrumbs->parent('admin');
-    $breadcrumbs->push('Merchant review', route('review.merchants.index'));
-});
-
-// Admin panel > Merchant reviews > [Merchant review]
-Breadcrumbs::register('merchant-review', function ($breadcrumbs, $merchantReview) {
-    $breadcrumbs->parent('merchant-reviews');
-    $breadcrumbs->push('Edit merchant review', route('review.merchants.view', ['id' => $merchantReview->id]));
-});
-
 // Admin panel > Income payments
 Breadcrumbs::register('income-payments', function ($breadcrumbs) {
     $breadcrumbs->parent('admin');
     $breadcrumbs->push('Income payments', route('payments.income.index'));
 });
+
 // Admin panel > Income payments > Income details
 Breadcrumbs::register('income-payment', function ($breadcrumbs, $order) {
     $breadcrumbs->parent('income-payments');
@@ -118,34 +95,4 @@ Breadcrumbs::register('outcome-payment-create', function ($breadcrumbs) {
 Breadcrumbs::register('outcome-payment', function ($breadcrumbs, $outcome) {
     $breadcrumbs->parent('outcome-payments');
     $breadcrumbs->push('Outcome details', route('outcome.edit', ['id' => $outcome->id]));
-});
-
-// Admin panel > Ad mobs
-Breadcrumbs::register('adverts', function ($breadcrumbs) {
-    $breadcrumbs->parent('admin');
-    $breadcrumbs->push('Ad banners', route('adverts.index'));
-});
-
-// Admin panel > Ad mobs > Create ad mob
-Breadcrumbs::register('advert-create', function ($breadcrumbs) {
-    $breadcrumbs->parent('adverts');
-    $breadcrumbs->push('Banner management', route('adverts.create'));
-});
-
-// Admin panel > Ad mobs > Edit ad mob
-Breadcrumbs::register('advert', function ($breadcrumbs, $advert) {
-    $breadcrumbs->parent('adverts');
-    $breadcrumbs->push('Banner management', route('adverts.edit', ['id' => $advert->id]));
-});
-
-// Admin panel > Logs
-Breadcrumbs::register('logs', function ($breadcrumbs) {
-    $breadcrumbs->parent('admin');
-    $breadcrumbs->push('Logs', route('logs.index'));
-});
-
-// Admin panel > Csv
-Breadcrumbs::register('csv', function ($breadcrumbs) {
-    $breadcrumbs->parent('admin');
-    $breadcrumbs->push('Csv', route('csv.index'));
 });
