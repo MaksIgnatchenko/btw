5c1b2ce8d5ee132024e2e9f3f2e1c20e1a15d9d5
ilyakobus <ilya.kobus@appus.me>
2018-11-15 15:27:05 +0200

    [#] Replace duplicated requests code to abstraction

--- app/Modules/Products/Requests/Web/EditProductRequest.php
+++ app/Modules/Products/Requests/Web/EditProductRequest.php
@@ -5,9 +5,7 @@
 
 namespace App\Modules\Products\Requests\Web;
 
-use Illuminate\Foundation\Http\FormRequest;
-
-class EditProductRequest extends FormRequest
+class EditProductRequest extends UpdateProductRequestAbstract
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -26,17 +24,9 @@ class EditProductRequest extends FormRequest
      */
     public function rules(): array
     {
-        return [
-            'category_id' => 'required|exists:categories,id',
-            'attributes' => 'sometimes|array',
-            'name' => 'required|string|max:50',
-            'description' => 'required|string|max:1000',
-            'quantity' => 'required|numeric|between:0,9999',
-            'main_image' => 'mimes:' . config('wish.storage.products.image_mimes') . '|max:' . config('wish.storage.products.image_max_size'),
-            'product_gallery.*' => 'mimes:' . config('wish.storage.products.image_mimes') . '|max:' . config('wish.storage.products.image_max_size'),
-            'price' => 'required|numeric|between:0.01,9999999',
-            'attributes.text.*' => 'required|string|max:100',
-            'attributes.digits.*' => 'required|integer',
+        return parent::rules() + [
+            'main_image' => 'mimes:' . config('wish.storage.products.image_mimes') . '|max:'
+                . config('wish.storage.products.image_max_size'),
         ];
     }
 }
