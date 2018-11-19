8f51f8eb7d17bd8a1a049e165d25d5c5d1512dac
ilyakobus <ilya.kobus@appus.me>
2018-11-19 17:16:42 +0200

    [!] Fix wrong path to IMask

--- app/Modules/Users/Merchant/Resources/views/merchants/web/country-info.blade.php
+++ app/Modules/Users/Merchant/Resources/views/merchants/web/country-info.blade.php
@@ -2,7 +2,7 @@
 
 @section('script')
     <script src="{{asset('js/marchants/registration/contact-data.js')}}"></script>
-    <script src="{{asset('js/vendor/imask.js')}}"></script>
+    <script src="{{asset('vendor/imask.js')}}"></script>
 @endsection
 
 @section('title', __('merchants.page_titles.contact_info'))
