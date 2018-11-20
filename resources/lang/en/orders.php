9afa47600f164a2b97c457a572a62f7fe44d901b
Andrei Podgornyi <andrei.podgornyi@appus.me>
2018-11-20 16:35:44 +0200

    [+] add multilang file for orders

---
+++ resources/lang/en/orders.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by Andrei Podgornyi, Appus Studio LP on 19.11.2018
+ */
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Pagination Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the paginator library to build
+    | the simple pagination links. You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+
+    'view_order' => 'View',
+    'table_head' => [
+        'order' => 'Order #',
+        'purchase_date' => 'Purchase date',
+        'customer' => 'Customer',
+        'amount' => 'Amount',
+        'status' => 'Status',
+    ],
+    'order_details' => [
+        'order' => ' Order',
+        'product' => 'Product',
+        'price' => 'Price',
+        'purchase_date' => 'Purchase date',
+        'quantity' => 'Quantity',
+        'amount' => 'Amount',
+        'status' => 'Status',
+        'change_status_description' => '«Shipped» - You can mark an order as shipped after you transfer it to the customer through the shipping service.',
+    ],
+];
