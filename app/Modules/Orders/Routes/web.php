8c9d338001a1f72c9b5dde80c615ae37b8730e6e
Andrei Podgornyi <andrei.podgornyi@appus.me>
2018-11-20 19:29:05 +0200

    [#] refactor order moudule routs

--- app/Modules/Orders/Routes/web.php
+++ app/Modules/Orders/Routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
-Route::get('orders/search', 'SearchController@index')->middleware(['auth:merchant'])->name('orders.search');
-
 $this->group([
     'middleware' => ['auth:merchant'],
 ], function () {
+    Route::get('orders/search', 'SearchController@index')->name('orders.search');
     Route::resource('orders', 'OrderController', ['as' => 'web'])
         ->only(['index', 'show', 'update']);
 });
