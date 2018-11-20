5c9293e9e5fcdbde7653ed4ed740cda6c39f7bf4
ilyakobus <ilya.kobus@appus.me>
2018-11-20 18:36:38 +0200

    [#] Update nav links and i18n

--- resources/views/layouts/merchants/navigation.blade.php
+++ resources/views/layouts/merchants/navigation.blade.php
@@ -8,7 +8,7 @@
             <a href="#" class="nav-line__link @if ($active === 'orders') nav-line__link--active @endif">{{__('store.my_orders')}}</a>
         </li>
         <li class="nav-line__item">
-            <a href="#" class="nav-line__link @if ($active === 'settings') nav-line__link--active @endif">{{__('store.settings')}}</a>
+            <a href="{{ route('merchant.settings') }}" class="nav-line__link @if ($active === 'settings') nav-line__link--active @endif">{{__('store.settings')}}</a>
         </li>
     </ul>
 </div><!-- /. end navigate link -->
