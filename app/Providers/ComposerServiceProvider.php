ba0855bea3d00acee095074e17a00ab07d8ff318
ilyakobus <ilya.kobus@appus.me>
2018-11-20 18:33:14 +0200

    [+#] Add password rule, update view composer

--- app/Providers/ComposerServiceProvider.php
+++ app/Providers/ComposerServiceProvider.php
@@ -18,6 +18,6 @@ class ComposerServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        View::composer(['products.web.index', 'products.web.create'], GlobalComposer::class);
+        View::composer(['*.web.*', 'merchants.auth.*'], GlobalComposer::class);
     }
 }
