1dcb669cd2f8b2ab9d353a1f26335ba09c2e8540
Andrei Podgornyi <andrei.podgornyi@appus.me>
2018-11-27 18:29:12 +0200

    [+] loading animation implementation

--- public/js/Validator.js
+++ public/js/Validator.js
@@ -51,11 +51,20 @@ var Validator = function (form, fields) {
         var newElem = $('<div>')
             .addClass('alert')
             .addClass('alert-danger')
-            .attr('role', 'alert')
-            .append(
+            .attr('role', 'alert');
+
+        if (fieldName == 'category') {
+            newElem.append(
+                $('<strong>')
+                    .html(`Please select a category.`)
+            );
+        } else {
+            newElem.append(
                 $('<strong>')
                     .html(`The ${fieldName} field is required`)
             );
+        }
+
         element.after(newElem);
         $('html, body').animate({
             scrollTop: newElem.offset().top - 50
--- public/js/custom.js
+++ public/js/custom.js
@@ -293,7 +293,7 @@ window.onload = function () {
             }
         }
 
-        function onProductFormSubmit(event, elements) {
+        function onProductFormSubmit(event, elements, submitButton) {
             event.preventDefault();
             /* Clear disable attribute on input type file before send form */
             for (var i = 0; i < elements.length; i++) {
@@ -325,8 +325,14 @@ window.onload = function () {
             var validator = new Validator(event.target, validationFields);
             if (validator.validate()) {
 
-                $('.create-product.submit').prop('disabled', true);
-                $('.create-product.submit').addClass('loading');
+                if (document.querySelector('.create-product.submit')) {
+                    disableAndAnimate($('.create-product.submit'));
+                }
+
+                if (document.querySelector('.edit-product.submit')) {
+                    disableAndAnimate($('.edit-product.submit'));
+                }
+
 
                 event.target.submit();
             }
@@ -485,6 +491,7 @@ window.onload = function () {
             document.querySelector('form[name="change-store"]').addEventListener('submit', function (event) {
                 event.preventDefault();
                 fileEl.removeAttribute('disabled');
+                disableAndAnimate($('.store-settings.submit'));
                 event.target.submit();
             });
         }
