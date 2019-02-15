b2a440607383428b1ed2700afd966348bec3d044
Viacheslav Bilohlazov <viacheslav.bilohlazov@appus.me>
2019-02-15 17:10:49 +0200

    [#] added comma in CreateReviewRequest

--- app/Modules/Reviews/Requests/CreateReviewRequest.php
+++ app/Modules/Reviews/Requests/CreateReviewRequest.php
@@ -43,7 +43,7 @@ class CreateReviewRequest extends FormRequest
                 'required',
                 'integer',
                 new NotRatedOrderRule(),
-                new PickedUpOrderRule()
+                new PickedUpOrderRule(),
             ]
         ];
     }
