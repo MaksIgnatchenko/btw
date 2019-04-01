2f8593afb53b9aa12a40166d67d82d0129d2662b
Viacheslav Bilohlazov <viacheslav.bilohlazov@appus.me>
2019-04-01 12:15:29 +0300

    trying test configuration

---
+++ init_test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+chmod 777 -R storage/ bootstrap/cache
+
+composer install
+composer dump-autoload
+
+cp .env.testing .env
+
+rm -f ./public/storage
+ln -s ../storage/app/public/ ./public/storage
+
+php phpunit.phar
